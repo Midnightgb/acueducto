0dc0e523f2dc4e1985a1477639a804910b27fb3d
Damaez <david2013mip@gmail.com>
2023-10-24 22:30:55 -0500

    reiniciando rama

--- Acueducto/public/dist/assets/js/select.js
+++
@@ -1,5 +0,0 @@
-let formulario = document.getElementById('formulario');
-let selectEmpresa = document.getElementById('selectEmpresa');
-selectEmpresa.addEventListener('change', () => {
-    formulario.submit();
-});
