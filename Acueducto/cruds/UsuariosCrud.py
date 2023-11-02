e776c0d53fafac711f2d46705ccf451009387928
roman09wS <winderroman3131@gmail.com>
2023-11-02 11:41:10 -0500

    corregido listaAsistentes

--- Acueducto/cruds/UsuariosCrud.py
+++ Acueducto/cruds/UsuariosCrud.py
@@ -564,11 +564,24 @@ def obtenerSuscriptoresEmpresa(
             headers = elimimar_cache()
             reuniones = obtenerReuAdmin(usuario.empresa, db)
             if query_usuarios:
+
                 query_asistentes = db.query(ListaAsistencia).filter(ListaAsistencia.id_reunion == reunion_1).all()
-                lista_combinada = zip(query_usuarios,query_asistentes)
+
+                lista_combinada = {"suscriptor":[]}
+
+                for busquedaUsuarios in query_usuarios:
+                    estado = False
+                    for usuariosReunion in query_asistentes:
+                        if busquedaUsuarios.id_usuario == usuariosReunion.id_usuario:
+                            lista_combinada["suscriptor"].append([busquedaUsuarios,True])
+                            estado = True
+                            break
+                    if not estado:
+                        lista_combinada["suscriptor"].append([busquedaUsuarios,False])
+
                 response = template.TemplateResponse(
                     "paso-1/paso1-2/llamado_lista.html",
-                    {"request": request, "lista_combinada": lista_combinada, "usuario": usuario,
+                    {"request": request, "usuarios": lista_combinada, "usuario": usuario,
                         "reuniones": reuniones, "reunionSelect": reunion_select},
                 )
                 response.headers.update(headers)
