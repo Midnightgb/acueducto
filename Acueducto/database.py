50731578a0b476c12c1b1c93b12d5e1661ac0e96
JP_Tapasco <restrepofreddy28@gmail.com>
2023-11-08 11:33:46 -0500

    estatutos melos

---
+++ Acueducto/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# DATABASE_URL = "mysql+pymysql:/manolos740:Manolos22!@207.246.65.47/acueducto"
+#DATABASE_URL = "mysql+pymysql://adso:Manolosyes!!@45.77.161.32:3306/acueducto"
+DATABASE_URL = "mysql+pymysql://root:@localhost/acueducto"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_database():
+    database = SessionLocal()
+    try:
+        yield database
+    finally:
+        database.close()
